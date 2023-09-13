bf97ee4cf8dc1af011ae4f97cb0d2b853a49e97c
Muhammad Hamza Shaikh <hamzashaikhwork@gmail.com>
2023-09-13 09:12:53 +0500

    feat: Redesign Coming soon page

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen flex flex-col justify-between bg-gray-50 dark:bg-gray-900">
       <Header />
       <ComingSoon />
       <Footer />
-    </>
+    </div>
   );
 }
--- src/components/Comingsoon/ComingSoon.jsx
+++ src/components/Comingsoon/ComingSoon.jsx
@@ -3,154 +3,131 @@ import { IMAGES } from "@/assets/images";
 import { SVG } from "@/assets/SVGs";
 
 function ComingSoon() {
+  const backgroundImage = `url(${IMAGES.bgImage})`;
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-12 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="place-self-center mr-auto mb-10 lg:col-span-7 xl:col-span-8 xl:mb-0">
-          <h1 class="mb-4 max-w-2xl text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Discover new product and best possibilities
+    // <section
+    //   style={{ backgroundImage: backgroundImage }}
+    //   className="bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply bg-opacity-60"
+    // >
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="grid justify-center py-8 px-4 mx-auto max-w-screen-xl lg:gap-12 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="place-self-center  mr-auto mb-10 lg:col-span-7 xl:col-span-8 xl:mb-0">
+          <h1 className="mb-6 max-w-2xl text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            Coming Soon
           </h1>
-          <p class="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value and drive economic growth.
+          <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Signup to get updates
           </p>
-          <a
-            href="#"
-            class="inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            <svg
-              class="mr-2 -ml-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-            </svg>
-            Watch video
-          </a>
-          <ul class="hidden justify-between pt-12 mx-auto mt-14 border-t border-gray-300 xl:flex dark:border-gray-700 dark:text-white">
-            <li class="flex">
-              <span class="text-4xl font-extrabold lg:text-5xl">42k</span>
-              <div class="block pl-4 text-xl text-gray-500 dark:text-gray-400">
-                <div>Our Active</div>
-                <div>Users</div>
-              </div>
-            </li>
-            <li class="flex">
-              <span class="text-4xl font-extrabold lg:text-5xl">3k</span>
-              <div class="block pl-4 text-xl text-gray-500 dark:text-gray-400">
-                <div>Professional</div>
-                <div>Creators</div>
-              </div>
-            </li>
-            <li class="flex">
-              <span class="text-4xl font-extrabold lg:text-5xl">560k</span>
-              <div class="block pl-4 text-xl text-gray-500 dark:text-gray-400">
-                <div>Weekly</div>
-                <div>Downloads</div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="justify-center p-4 max-w-screen-sm bg-white rounded-lg border border-gray-200 shadow lg:mt-0 lg:col-span-5 xl:col-span-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form class="space-y-6" action="#">
-            <h2 class="text-xl font-medium text-gray-900 dark:text-white">
-              Join over 3,000 creators
-            </h2>
-            <div class="flex items-center space-x-4">
-              <a
-                href="#"
-                class="w-full text-white bg-[#4284F4] hover:bg-[#3372df] dark:focus:ring-[#0f53c9] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Google
-              </a>
-              <a
-                href="#"
-                class="w-full text-white bg-[#EA4C89] hover:bg-[#d8437c] dark:focus:ring-[#9c0c43] focus:ring-4 focus:outline-none focus:ring-[#f39cbe] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Dribbble
-              </a>
-            </div>
-            <div class="flex items-center">
-              <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
-              <div class="px-5 text-center text-gray-500 dark:text-gray-400">
-                or
-              </div>
-              <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
-            </div>
-            <div>
-              <label
-                for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="name@flowbite.com"
-                required=""
-              />
-            </div>
-            <div>
-              <label
-                for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Your password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                required=""
-              />
-            </div>
-            <div class="flex items-start">
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div class="ml-3 text-sm">
-                  <label
-                    for="remember"
-                    class="font-medium text-gray-900 dark:text-gray-300"
+          <form action="#" className="">
+            <div className="justify-center items-center mx-auto mb-3 space-y-4 sm:flex lg:justify-start sm:space-y-0 sm:space-x-4">
+              <div className="relative">
+                <label
+                  for="email"
+                  className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Email address
+                </label>
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Remember me
-                  </label>
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                  </svg>
+                </div>
+                <input
+                  className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:w-80 xl:w-96 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Enter your email"
+                  type="email"
+                  id="email"
+                  required=""
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full sm:w-auto bg-[#30A55A]  justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign up
+                <svg
+                  className="ml-2 -mr-1 w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div className="mt-4 sm:border-t sm:border-gray-400 sm:mt-8 sm:pt-8 dark:border-gray-700">
+              <p className="hidden text-base font-medium text-gray-500 sm:block">
+                Follow us on:
+              </p>
+
+              <div className="flex items-center gap-2 mt-3 max-w-md">
+                <a
+                  href="#"
+                  data-tooltip-target="tooltip-facebook"
+                  className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+                >
+                  <svg
+                    className="w-8 h-8"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 8 19"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+
+                  <span className="sr-only">Facebook</span>
+                </a>
+                <div
+                  id="tooltip-facebook"
+                  role="tooltip"
+                  className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+                >
+                  Like us on Facebook
+                  <div className="tooltip-arrow" data-popper-arrow></div>
+                </div>
+                <a
+                  href="#"
+                  data-tooltip-target="tooltip-twitter"
+                  className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+                >
+                  <svg
+                    className="w-8 h-8"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z"
+                    />
+                  </svg>
+                  <span className="sr-only">Twitter</span>
+                </a>
+                <div
+                  id="tooltip-twitter"
+                  role="tooltip"
+                  className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+                >
+                  Follow us on Twitter
+                  <div className="tooltip-arrow" data-popper-arrow></div>
                 </div>
               </div>
-              <a
-                href="#"
-                class="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500"
-              >
-                Lost Password?
-              </a>
-            </div>
-            <button
-              type="submit"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-500 dark:focus:ring-primary-700"
-            >
-              Create an account
-            </button>
-            <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?{" "}
-              <a
-                href="#"
-                class="text-primary-700 hover:underline dark:text-primary-500"
-              >
-                Create account
-              </a>
             </div>
           </form>
         </div>
